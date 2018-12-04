bb2f3b25b8a7965c6119509dd76796c6ac58c6a5
roseee123 <43983964+roseee123@users.noreply.github.com>
2018-12-04 16:16:24 +0800

    commit20181204

---
+++ myjsfile.js
@@ -0,0 +1,8 @@
+function futurevalue()
+{
+	var pv=parseInt(document.getElementById("amount").value);
+	var i=parseFloat(document.getElementById("rate").value);
+	var n=parseInt(document.getElementById("period").value);
+	fv=pv*(Math.pow(1+i,n));
+	document.write("本利和="+fv+"</br>");
+}
